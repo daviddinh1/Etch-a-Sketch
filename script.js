709c24cff1637c32c8d7518988c94f140dc47d96
daviddinh1 <91696365+daviddinh1@users.noreply.github.com>
2023-06-18 18:32:21 -0500

    finished need to add learn how to add hold and click and erase button

--- script.js
+++ script.js
@@ -1,12 +1,48 @@
-const container = document.querySelector('.container');
+let color = 'rainbow';
 
+function createBoard(size){
+ let board = document.querySelector('.board');
+ let squares = board.querySelectorAll('div');
 
-//rows
-for(let i = 0; i<256; i++){
- const square = document.createElement('div');
- square.className = 'squares';
- container.appendChild(square);
+ squares.forEach((div) => div.remove());
+
+ board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+ board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  let amount = size * size;
+
+  for(let i = 0 ; i <amount; i++){
+   let square = document.createElement('div');
+   square.addEventListener('mouseover',changeColor);
+   square.style.backgroundColor = 'white';
+   board.insertAdjacentElement('beforeend',square);
+   
+ }
 }
+ 
+createBoard(16);
+
+function changeSize(input){
+ createBoard(input);
+}
+
+function colorChoice(choice){
+ color = choice;
+}
+
+function changeColor(){
+ if(color === 'rainbow'){
+  this.style.backgroundColor = `hsl(${Math.random() *360},100%,50%)`; //later on add a color wheel
+ }
+ else{
+  this.style.backgroundColor = 'black';
+ }
+}
+
+
+
+
+
 
 
 
